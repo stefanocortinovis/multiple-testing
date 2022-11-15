efa780b2623cc85f24dbf8542f78bcb3809ab974
Stefano Cortinovis <stefanocortinovis17@gmail.com>
2022-11-15 12:39:43 +0000

    Fix issue with hochberg and benjamini-hochberg when no pvalues satisfies rejection condition

--- src/algorithms.py
+++ src/algorithms.py
@@ -21,8 +21,10 @@ def hochberg(pvalues, alpha=0.05):
     pred = np.zeros_like(pvalues, dtype=int)
     ind = np.argsort(pvalues, axis=-1)
     pvalues_sorted = pvalues[np.arange(reps)[:, np.newaxis], ind]
-    i = pvalues.shape[1] - np.argmax((pvalues_sorted <= alpha / (m - np.arange(m)))[:, ::-1], axis=1) - 1 # last argmax in array
-    i = np.where(np.arange(m) <= i[:, np.newaxis])
+    condition = pvalues_sorted <= alpha / (m - np.arange(m))
+    i = pvalues.shape[1] - np.argmax(condition[:, ::-1], axis=1) # last argmax in array + 1 (keep only until i by using "<" later ***)
+    i[~condition[np.arange(reps), i - 1]] = 0 # TODO: maybe could be done more elegantly using np.nonzero()?
+    i = np.where(np.arange(m) < i[:, np.newaxis]) # ***
     pred[i[0], ind[i]] = 1
     return pred
 
@@ -32,7 +34,9 @@ def benjamini_hochberg(pvalues, alpha=0.05, independent=True):
     C = 1 if independent else np.sum(1 / np.arange(1, m + 1))
     ind = np.argsort(pvalues, axis=-1)
     pvalues_sorted = pvalues[np.arange(reps)[:, np.newaxis], ind]
-    i = pvalues.shape[1] - np.argmax((pvalues_sorted < alpha * (np.arange(m) + 1) / (C * m))[:, ::-1], axis=1) - 1 # last argmax in array
-    i = np.where(np.arange(m) <= i[:, np.newaxis])
+    condition = pvalues_sorted < alpha * (np.arange(m) + 1) / (C * m)
+    i = pvalues.shape[1] - np.argmax(condition[:, ::-1], axis=1) # last argmax in array + 1 (keep only until i by using "<" later at ***)
+    i[~condition[np.arange(reps), i - 1]] = 0 # TODO: maybe could be done more elegantly using np.nonzero()?
+    i = np.where(np.arange(m) < i[:, np.newaxis]) # ***
     pred[i[0], ind[i]] = 1
     return pred
