e82114bc82972c605b50ed846e33ce35403fc097
Stefano Cortinovis <stefanocortinovis17@gmail.com>
2022-11-15 12:41:58 +0000

    Implement FWE and FDP

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,16 @@ def get_t_statistics_z_test(m, n, reps, mus, mu0):
 
 def get_pvalues_z_test(t_statistics):
     return 2 * norm.cdf(-abs(t_statistics))
+
+def FWE(true, pred):
+    false_reject = pred.copy()
+    false_reject *= (1 - true)
+    return np.any(false_reject, axis=-1).astype(int)
+
+def FDP(true, pred):
+    total_reject = pred.sum(axis=-1)
+    total_reject[total_reject == 0] = 1
+    false_reject = pred.copy()
+    false_reject *= (1 - true)
+    false_reject = false_reject.sum(axis=-1)
+    return false_reject / total_reject
