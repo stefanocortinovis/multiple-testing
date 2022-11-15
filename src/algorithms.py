3105eb81d6ba2d3ca492ae3e2db4674580e4f44a
Stefano Cortinovis <stefanocortinovis17@gmail.com>
2022-11-15 00:29:24 +0000

    Vectorize multiple hypothesis testing algorithms

--- src/algorithms.py
+++ src/algorithms.py
@@ -2,35 +2,37 @@ import numpy as np
 
 
 def bonferroni(pvalues, alpha=0.05):
-    m = len(pvalues)
-    pred = np.zeros(m, dtype=int)
+    m = pvalues.shape[1]
+    pred = np.zeros_like(pvalues, dtype=int)
     pred[pvalues < alpha / m] = 1
     return pred
 
 def holm_bonferroni(pvalues, alpha=0.05):
-    m = len(pvalues)
-    pred = np.zeros(m, dtype=int)
-    ind = np.argsort(pvalues)
-    pvalues_sorted = pvalues[ind]
-    i = (pvalues_sorted > alpha / (m - np.arange(m))).nonzero()[0][0]
-    pred[ind[:i]] = 1
+    reps, m = pvalues.shape
+    pred = np.zeros_like(pvalues, dtype=int)
+    ind = np.argsort(pvalues, axis=-1)
+    pvalues_sorted = pvalues[np.arange(reps)[:, np.newaxis], ind]
+    i = np.where(np.arange(m) < np.argmax(pvalues_sorted > alpha / (m - np.arange(m)), axis=1)[:, np.newaxis])
+    pred[i[0], ind[i]] = 1
     return pred
 
 def hochberg(pvalues, alpha=0.05):
-    m = len(pvalues)
-    pred = np.zeros(m, dtype=int)
-    ind = np.argsort(pvalues)
-    pvalues_sorted = pvalues[ind]
-    i = (pvalues_sorted <= alpha / (m - np.arange(m))).nonzero()[0][-1]
-    pred[ind[:i+1]] = 1
+    reps, m = pvalues.shape
+    pred = np.zeros_like(pvalues, dtype=int)
+    ind = np.argsort(pvalues, axis=-1)
+    pvalues_sorted = pvalues[np.arange(reps)[:, np.newaxis], ind]
+    i = pvalues.shape[1] - np.argmax((pvalues_sorted <= alpha / (m - np.arange(m)))[:, ::-1], axis=1) - 1 # last argmax in array
+    i = np.where(np.arange(m) <= i[:, np.newaxis])
+    pred[i[0], ind[i]] = 1
     return pred
 
 def benjamini_hochberg(pvalues, alpha=0.05, independent=True):
-    m = len(pvalues)
-    pred = np.zeros(m, dtype=int)
+    reps, m = pvalues.shape
+    pred = np.zeros_like(pvalues, dtype=int)
     C = 1 if independent else np.sum(1 / np.arange(1, m + 1))
-    ind = np.argsort(pvalues)
-    pvalues_sorted = pvalues[ind]
-    i = (pvalues_sorted < alpha * (np.arange(m) + 1) / (C * m)).nonzero()[0][-1]
-    pred[ind[:i+1]] = 1
+    ind = np.argsort(pvalues, axis=-1)
+    pvalues_sorted = pvalues[np.arange(reps)[:, np.newaxis], ind]
+    i = pvalues.shape[1] - np.argmax((pvalues_sorted < alpha * (np.arange(m) + 1) / (C * m))[:, ::-1], axis=1) - 1 # last argmax in array
+    i = np.where(np.arange(m) <= i[:, np.newaxis])
+    pred[i[0], ind[i]] = 1
     return pred
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 alpha = 0.05
-pvalues = np.array([
+pvalues = np.array([[
     0.1055,
     0.0045,
     0.0090,
@@ -15,21 +15,26 @@ pvalues = np.array([
     0.0085,
     0.0080,
     0.0175
-])
+]])
 pvalues_sorted = np.sort(pvalues)
+pvalues_stacked = np.vstack((pvalues, pvalues_sorted))
 
 def test_bonferroni():
-    assert np.all(bonferroni(pvalues, alpha) == np.array([0, 1, 0, 1, 0, 0, 0, 0, 0, 0]))
-    assert np.all(bonferroni(pvalues_sorted, alpha) == np.array([1, 1, 0, 0, 0, 0, 0, 0, 0, 0]))
+    assert np.all(bonferroni(pvalues, alpha) == np.array([[0, 1, 0, 1, 0, 0, 0, 0, 0, 0]]))
+    assert np.all(bonferroni(pvalues_sorted, alpha) == np.array([[1, 1, 0, 0, 0, 0, 0, 0, 0, 0]]))
+    assert np.all(bonferroni(pvalues_stacked, alpha) == np.array([[0, 1, 0, 1, 0, 0, 0, 0, 0, 0], [1, 1, 0, 0, 0, 0, 0, 0, 0, 0]]))
 
 def test_holm_bonferroni():
-    assert np.all(holm_bonferroni(pvalues, alpha) == np.array([0, 1, 0, 1, 0, 1, 0, 0, 0, 0]))
-    assert np.all(holm_bonferroni(pvalues_sorted, alpha) == np.array([1, 1, 1, 0, 0, 0, 0, 0, 0, 0]))
+    assert np.all(holm_bonferroni(pvalues, alpha) == np.array([[0, 1, 0, 1, 0, 1, 0, 0, 0, 0]]))
+    assert np.all(holm_bonferroni(pvalues_sorted, alpha) == np.array([[1, 1, 1, 0, 0, 0, 0, 0, 0, 0]]))
+    assert np.all(holm_bonferroni(pvalues_stacked, alpha) == np.array([[0, 1, 0, 1, 0, 1, 0, 0, 0, 0], [1, 1, 1, 0, 0, 0, 0, 0, 0, 0]]))
 
 def test_hochberg():
-    assert np.all(hochberg(pvalues, alpha) == np.array([0, 1, 1, 1, 0, 1, 0, 1, 1, 0]))
-    assert np.all(hochberg(pvalues_sorted, alpha) == np.array([1, 1, 1, 1, 1, 1, 0, 0, 0, 0]))
+    assert np.all(hochberg(pvalues, alpha) == np.array([[0, 1, 1, 1, 0, 1, 0, 1, 1, 0]]))
+    assert np.all(hochberg(pvalues_sorted, alpha) == np.array([[1, 1, 1, 1, 1, 1, 0, 0, 0, 0]]))
+    assert np.all(hochberg(pvalues_stacked, alpha) == np.array([[0, 1, 1, 1, 0, 1, 0, 1, 1, 0], [1, 1, 1, 1, 1, 1, 0, 0, 0, 0]]))
 
 def test_benjamini_hochberg():
-    assert np.all(benjamini_hochberg(pvalues, alpha) == np.array([0, 1, 1, 1, 1, 1, 0, 1, 1, 1]))
-    assert np.all(benjamini_hochberg(pvalues_sorted, alpha) == np.array([1, 1, 1, 1, 1, 1, 1, 1, 0, 0]))
+    assert np.all(benjamini_hochberg(pvalues, alpha) == np.array([[0, 1, 1, 1, 1, 1, 0, 1, 1, 1]]))
+    assert np.all(benjamini_hochberg(pvalues_sorted, alpha) == np.array([[1, 1, 1, 1, 1, 1, 1, 1, 0, 0]]))
+    assert np.all(benjamini_hochberg(pvalues_stacked, alpha) == np.array([[0, 1, 1, 1, 1, 1, 0, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 0, 0]]))
